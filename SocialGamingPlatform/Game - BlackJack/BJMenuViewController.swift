20d3e68be4b81a31be99b021b91501bd8b07b78d
Dennis Lin <txl429@case.edu>
2018-10-27 16:36:50 -0400

    Added BlackJack Splash Screen/Menu

---
+++ SocialGamingPlatform/Game - BlackJack/BJMenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BJMenuViewController.swift
+//  SocialGamingPlatform
+//
+//  Created by Dennis Lin on 10/27/18.
+//  Copyright © 2018 Jiahao Luo. All rights reserved.
+//
+
+import UIKit
+
+class BJMenuViewController: UIViewController {
+    
+    @IBOutlet weak var playBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func proceedToGame(_ sender: UIButton) {
+        
+    }
+}
