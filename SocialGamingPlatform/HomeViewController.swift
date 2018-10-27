3bb0b96cd46f145a1989ca724c16268b239ce1bc
Adam Luo <jxl1305@case.edu>
2018-10-27 15:21:28 -0400

    Sign up and login finished

--- SocialGamingPlatform/HomeViewController.swift
+++ SocialGamingPlatform/HomeViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("you are home!")
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func logout(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initial = storyboard.instantiateInitialViewController()
+        UIApplication.shared.keyWindow?.rootViewController = initial
+    }
+    
     /*
     // MARK: - Navigation
 
