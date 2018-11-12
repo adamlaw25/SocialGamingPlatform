0f34b8448bb676c235dd42ab49167d9ebc763a1d
Dennis Lin <txl429@case.edu>
2018-11-12 13:01:57 -0500

    created the game menu vew controller script

---
+++ SocialGamingPlatform/Controller/GameController/Connect4GameController/Connect4MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Connect4MenuViewController.swift
+//  SocialGamingPlatform
+//
+//  Created by Dennis Lin on 11/12/18.
+//  Copyright © 2018 Jiahao Luo. All rights reserved.
+//
+
+import UIKit
+
+class Connect4MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
