919219a89b6c9401752402df6462c0c5db3afd84
Dennis Lin <txl429@case.edu>
2018-11-17 16:11:14 -0500

    Fully implemented Slider puzzle with working moves counter and difficulty calculation algorithm

--- SocialGamingPlatform/Controller/GameController/SliderGameController/SliderGameController.swift
+++ SocialGamingPlatform/Controller/GameController/SliderGameController/SliderGameController.swift
@@ -8,30 +8,70 @@
 
 import Foundation
 
+enum SliderGameState: Int {
+    case in_progress = 0, gameover
+}
+
 class SliderGameController {
     
     var board : [[Int]]
-    
     var blanktile_row : Int
-    
     var blanktile_column : Int
-    
     var numOfMoves : Int
+    var score: Int
+    var game_state : SliderGameState
+    var score_multipler : Int
     
     init() {
         self.board = [[1,2,3], [4,5,6], [7,8,0]]
         self.blanktile_row = 2
         self.blanktile_column = 2
         self.numOfMoves = 0
+        self.score = 0
+        self.game_state = .in_progress
+        self.score_multipler = 1
     }
     
-    func shuffle() {
-        let directions = ["UP", "DOWN", "LEFT", "DOWN"]
-        let random_number  = Int.random(in: 0...3)
-        for _ in 0...20 {
+    func shuffle(n: Int) {
+        print(n)
+        var directions = getCurrentAvailableMoves()
+        var random_number  = Int.random(in: 0..<directions.count)
+        for _ in 0..<n {
             move(direction: directions[random_number])
             updateBlankTile()
+            directions = getCurrentAvailableMoves()
+            random_number = Int.random(in: 0..<directions.count)
         }
+    }
+    
+    private func getCurrentAvailableMoves() -> [String] {
+        var directions = [String]()
+        if blanktile_row > 0 {
+            directions.append("UP")
+        }
+        if blanktile_row < 2 {
+            directions.append("DOWN")
+        }
+        if blanktile_column > 0 {
+            directions.append("LEFT")
+        }
+        if blanktile_column < 2 {
+            directions.append("RIGHT")
+        }
+        return directions
+    }
+    
+    func resetGame() {
+        self.board = [[1,2,3], [4,5,6], [7,8,0]]
+        let difficulty_index = calculateDifficulty()
+        self.shuffle(n: difficulty_index)
+        self.updateBlankTile()
+        self.numOfMoves = 0
+        self.game_state = .in_progress
+    }
+    
+    private func calculateDifficulty() -> Int {
+        return 5 + score/50
     }
     
     func move(direction: String) {
@@ -72,6 +112,7 @@ class SliderGameController {
             //throw error
             break
         }
+        updateBlankTile()
     }
     
     func hasWon() -> Bool {
@@ -91,6 +132,36 @@ class SliderGameController {
         }
     }
     
+    private func awardScore() {
+        self.score += ((100 - numOfMoves) * score_multipler)
+    }
+    
+    func gameOverNotification() {
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "Slider Game Over"), object: self, userInfo: ["numOfMoves": numOfMoves])
+    }
+    
+    func updateGameState() {
+        switch game_state {
+        case .in_progress:
+            if hasWon() {
+                updateToGameOver()
+            }
+            break
+        case .gameover:
+            gameoverAction()
+            break
+        }
+    }
+    
+    private func updateToGameOver() {
+        game_state = .gameover
+        gameoverAction()
+    }
+    
+    private func gameoverAction() {
+        awardScore()
+        gameOverNotification()
+    }
     
     
     //for debugging purposes: print the gameboard
@@ -100,11 +171,4 @@ class SliderGameController {
         }
         print("\n")
     }
-    
-    
-    
-    
-    
-    
-    
 }
