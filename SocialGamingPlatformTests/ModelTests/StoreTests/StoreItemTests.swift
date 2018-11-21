0fe0fae9181a1407f6f0b53e9be7065c56918f2a
Adam Luo <jxl1305@case.edu>
2018-11-21 13:38:37 -0500

    StoreItem tests added

--- SocialGamingPlatformTests/ModelTests/StoreTests/StoreItemTests.swift
+++ SocialGamingPlatformTests/ModelTests/StoreTests/StoreItemTests.swift
@@ -11,24 +11,26 @@ import XCTest
 
 class StoreItemTests: XCTestCase {
 
+    var test_storeitem1: StoreItem!
+    var test_storeitem2: StoreItem!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        test_storeitem1 = StoreItem(name: "test", price: 10, detail: nil, power: Powerup(multiplier: 20, timeLimit: 30))
+        test_storeitem2 = StoreItem(name: "test2", price: 20, detail: "detail", power: nil)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_init() {
+        XCTAssertEqual(test_storeitem1.name, "test")
+        XCTAssertEqual(test_storeitem1.price, 10)
+        XCTAssertEqual(test_storeitem1.detail, "multiplier: 20\ntime limit: 30")
+        XCTAssertEqual(test_storeitem1.powerUp?.multiplier, 20)
+        XCTAssertEqual(test_storeitem1.powerUp?.timeLimit, 30)
+        XCTAssertEqual(test_storeitem2.detail, "detail")
     }
 
 }
